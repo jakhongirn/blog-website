b2d5aea5ea82e10935eb1ab4abb0bcd280209d99
jaykhans <jahongirnagmatov@gmail.com>
2022-04-05 22:30:48 +0500

    update home

--- app.js
+++ app.js
@@ -21,12 +21,7 @@ const postsSchema = new mongoose.Schema({
   date: Date
 });
 
-const homeContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
 
 const Post = new mongoose.model("Post", postsSchema);
 
@@ -37,9 +32,7 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   Post.find({}, function (err, post) {
-    res.render("home", {
-      homeStartingContent: homeContent,
-    });
+    res.render("home");
   });
 });
 
@@ -52,11 +45,11 @@ app.get("/blog", function (req, res) {
 });
 
 app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent });
+  res.render("about");
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
+  res.render("contact");
 });
 
 app.get("/create", function (req, res) {
@@ -68,6 +61,8 @@ app.post("/create", function (req, res) {
   let postDB = new Post({
     title: req.body.blogTitle,
     content: req.body.blogBody,
+    date: req.body.blogDate,
+    author: req.body.blogAuthor
   });
 
   postDB.save();
@@ -86,6 +81,8 @@ app.get("/update/:id", function (req, res) {
         id: post._id,
         title: post.title,
         content: post.content,
+        author: post.author,
+        date: post.date
       });
     }
   });
@@ -95,7 +92,7 @@ app.post("/update/:id", function (req, res) {
   let requestedPostId = req.params.id;
   Post.findByIdAndUpdate(
     { _id: requestedPostId },
-    { title: req.body.blogTitle, content: req.body.blogBody },
+    { title: req.body.blogTitle, content: req.body.blogBody, date: req.body.blogDate, author: req.body.blogAuthor },
     function (err) {
       if (!err) {
         console.log("Successfully updated the blog.");
@@ -143,18 +140,7 @@ app.post("/delete/:id", function (req, res) {
   });
   res.redirect("/blog");
 });
-app.put("/update/:id", function (req, res) {
-  let requestedPostId = req.params.id;
-  Post.update(
-    { title: requestedPostTitle },
-    { title: req.body.title, content: req.body.content },
-    function (err) {
-      if (!err) {
-        res.send("Successfully updated the data.");
-      }
-    }
-  );
-});
+
 
 app.listen(PORT, function () {
   console.log(`Server started on port ${PORT}`);
